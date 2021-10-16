139636921ac70d71bc42270c09a39a4c8df2bcf1
a01745219EduardoRMuller <a01745219@itesm.mx>
2021-10-15 19:26:16 -0500

    lalaland

---
+++ SpecificNodes.cs
@@ -0,0 +1,22 @@
+namespace Falak
+{
+    public class SpecificNodes
+    {
+        class Program : Node { }
+
+        class defList : Node { }
+        class varDef: Node()
+        class idList: Node()
+        class idListCont: Node()
+        class funDef: Node()
+        class varDefList: Node()
+        class stmtList: Node()
+        class stmtIncr: Node()
+        class stmtDecr: Node()
+        class exprList: Node()
+        class exprListCont: Node()
+        class stmtIf: Node()
+        class elseIfList: Node()
+        class stmtElse: Node()
+    }
+}
