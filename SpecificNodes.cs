505d2901ec49d5d65530d521ab7fda536d1e6eb4
SebastianMoralesMartin <sebfut@hotmail.com>
2021-10-15 19:27:34 -0500

    key replacement

--- SpecificNodes.cs
+++ SpecificNodes.cs
@@ -5,18 +5,18 @@ namespace Falak
         class Program : Node { }
 
         class defList : Node { }
-        class varDef: Node()
-        class idList: Node()
-        class idListCont: Node()
-        class funDef: Node()
-        class varDefList: Node()
-        class stmtList: Node()
-        class stmtIncr: Node()
-        class stmtDecr: Node()
-        class exprList: Node()
-        class exprListCont: Node()
-        class stmtIf: Node()
-        class elseIfList: Node()
-        class stmtElse: Node()
+        class varDef: Node{ }
+        class idList: Node{ }
+        class idListCont: Node{ }
+        class funDef: Node{ }
+        class varDefList: Node{ }
+        class stmtList: Node{ }
+        class stmtIncr: Node{ }
+        class stmtDecr: Node{ }
+        class exprList: Node{ }
+        class exprListCont: Node{ }
+        class stmtIf: Node{ }
+        class elseIfList: Node{ }
+        class stmtElse: Node{ }
     }
 }
