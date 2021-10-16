6f10771dc91930539ccd1536f78b0797ca1af15f
a01745219EduardoRMuller <a01745219@itesm.mx>
2021-10-15 19:32:07 -0500

    Clase SpecificNodes.cs terminada, pending revision

--- SpecificNodes.cs
+++ SpecificNodes.cs
@@ -18,5 +18,22 @@ namespace Falak
         class stmtIf: Node{ }
         class elseIfList: Node{ }
         class stmtElse: Node{ }
+        
+        class stmtWhile: Node{ }
+        class stmtDoWhile: Node{ }
+        class stmtBreak: Node{ }
+        class stmtReturn: Node{ }
+        class stmtEmpty: Node{ }
+        class expr: Node{ }
+        class exprOr: Node{ }
+        class exprAnd: Node{ }
+        class exprComp: Node{ }
+        class exprRel: Node{ }
+        class exprAdd: Node{ }
+        class exprMul: Node{ }
+        class exprUnary: Node{ }
+        class exprPrimary: Node{ }
+        class array: Node{ }
+        class lit: Node{ }
     }
 }
