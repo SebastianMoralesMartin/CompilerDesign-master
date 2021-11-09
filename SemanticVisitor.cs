1594f1b6b1a8cc0ad59a31708f43ca386464f038
Eduardo R. Müller <a01745219@itesm.mx>
2021-11-08 23:23:41 -0600

    Ya estan

--- SemanticVisitor.cs
+++ SemanticVisitor.cs
@@ -7,6 +7,186 @@
                 { TokenCategory.BOOL, Type.BOOL },
                 { TokenCategory.INT, Type.INT }
             };
-        
+        //-----------------------------------------------------------
+        public IDictionary<string, Type> Table {
+            get;
+            private set;
+        }
+
+        //-----------------------------------------------------------
+        public SemanticVisitor() {
+            Table = new SortedDictionary<string, Type>();
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Program node) {
+            Visit((dynamic) node[0]);
+            Visit((dynamic) node[1]);
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(DeclarationList node) {
+            VisitChildren(node);
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Declaration node) {
+
+            var variableName = node[0].AnchorToken.Lexeme;
+
+            if (Table.ContainsKey(variableName)) {
+                throw new SemanticError(
+                    "Duplicated variable: " + variableName,
+                    node[0].AnchorToken);
+
+            } else {
+                Table[variableName] =
+                    typeMapper[node.AnchorToken.Category];
+            }
+
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(StatementList node) {
+            VisitChildren(node);
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Assignment node) {
+
+            var variableName = node.AnchorToken.Lexeme;
+
+            if (Table.ContainsKey(variableName)) {
+
+                var expectedType = Table[variableName];
+
+                if (expectedType != Visit((dynamic) node[0])) {
+                    throw new SemanticError(
+                        "Expecting type " + expectedType
+                        + " in assignment statement",
+                        node.AnchorToken);
+                }
+
+            } else {
+                throw new SemanticError(
+                    "Undeclared variable: " + variableName,
+                    node.AnchorToken);
+            }
+
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Print node) {
+            // Print expects a node of type int or bool, so we can
+            // safely ignore the return type of this Visit call.
+            Visit((dynamic) node[0]);
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(If node) {
+            if (Visit((dynamic) node[0]) != Type.BOOL) {
+                throw new SemanticError(
+                    $"Expecting type {Type.BOOL} in conditional statement",
+                    node.AnchorToken);
+            }
+            VisitChildren(node[1]);
+            return Type.VOID;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Identifier node) {
+
+            var variableName = node.AnchorToken.Lexeme;
+
+            if (Table.ContainsKey(variableName)) {
+                return Table[variableName];
+            }
+
+            throw new SemanticError(
+                $"Undeclared variable: {variableName}",
+                node.AnchorToken);
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(IntLiteral node) {
+
+            var intStr = node.AnchorToken.Lexeme;
+            int value;
+
+            if (!Int32.TryParse(intStr, out value)) {
+                throw new SemanticError(
+                    $"Integer literal too large: {intStr}",
+                    node.AnchorToken);
+            }
+
+            return Type.INT;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(True node) {
+            return Type.BOOL;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(False node) {
+            return Type.BOOL;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Neg node) {
+            if (Visit((dynamic) node[0]) != Type.INT) {
+                throw new SemanticError(
+                    $"Operator - requires an operand of type {Type.INT}",
+                    node.AnchorToken);
+            }
+            return Type.INT;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(And node) {
+            VisitBinaryOperator('&', node, Type.BOOL);
+            return Type.BOOL;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Less node) {
+            VisitBinaryOperator('<', node, Type.INT);
+            return Type.BOOL;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Plus node) {
+            VisitBinaryOperator('+', node, Type.INT);
+            return Type.INT;
+        }
+
+        //-----------------------------------------------------------
+        public Type Visit(Mul node) {
+            VisitBinaryOperator('*', node, Type.INT);
+            return Type.INT;
+        }
+
+        //-----------------------------------------------------------
+        void VisitChildren(Node node) {
+            foreach (var n in node) {
+                Visit((dynamic) n);
+            }
+        }
+
+        //-----------------------------------------------------------
+        void VisitBinaryOperator(char op, Node node, Type type) {
+            if (Visit((dynamic) node[0]) != type ||
+                Visit((dynamic) node[1]) != type) {
+                throw new SemanticError(
+                    $"Operator {op} requires two operands of type {type}",
+                    node.AnchorToken);
+            }
+        }
     }
 }
