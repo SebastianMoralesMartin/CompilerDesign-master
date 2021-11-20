c24ba8457e1c92a4747a489b312c0263fc52e771
Sebastian Morales Martin <sebfut@hotmail.com>
2021-11-19 23:59:59 -0600

    Primera Visita

--- FirstVisitor.cs
+++ FirstVisitor.cs
@@ -13,10 +13,43 @@ namespace Falak
     {
         public bool mainPresent = false;
         public bool visitingMain = false;
+
+        Type tempContainer;
+
+        // Table creation
+
+        public LocalTable GlobalFunctionsTable{
+            get;
+            private set;
+        }
+
+        public GlobalVarTable GlobalVars{
+            get;
+            private set;
+        }
+
+
+        public FirstVisitor(){
+            GlobalFunctionsTable = new LocalTable();
+            GlobalVars = new GlobalVarTable();
+
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "printi", "true", "1", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "printc", "true", "1", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "prints", "true", "1", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "println", "true", "0", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "readi", "true", "0", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "reads", "true", "0", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "new", "true", "1", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "size", "true", "1", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "add", "true", "2", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "get", "true", "2", "null" }));
+            GlobalFunctionsTable.Add(new Type(new List<object>() { "set", "true", "3", "null" }));
+        }
+
         
         //TODO: Crear tabla VGST
         
-        public IDictionary<string, FunCollection> FGST =
+        /*public IDictionary<string, FunCollection> FGST =
             new SortedDictionary<string, FunCollection>() {
                 { "printI", new FunCollection(true, 1, null)},
                 { "printC", new FunCollection(true, 1, null)},
@@ -29,7 +62,7 @@ namespace Falak
                 { "add", new FunCollection( true, 2, null)},
                 { "get", new FunCollection(true, 2, null)},
                 { "set", new FunCollection(true, 3, null)},
-            };
+            };*/
 
         public void Visit(Program node)
         {
@@ -56,17 +89,64 @@ namespace Falak
                 mainPresent = true;
                 visitingMain = true;
             }
-            if (Table.ContainsKey(functionName)) {
+            if (GlobalFunctionsTable.Contains(functionName)) {
                 throw new SemanticError(
                     "Duplicated function: " + functionName,
                     node.AnchorToken);
 
             } else {
-                int idListCount = 0;
-                FGST[functionName] = new FunCollection(false, idListCount, null);
-                VisitChildren(node);
+
+                tempContainer = new Type(new List<object>(){functionName, "false"});
+                //int idListCount = 0;
+                //FGST[functionName] = new FunCollection(false, idListCount, null);
+                Visit((dynamic) node[0]);
             }
 			
+        }
+        
+        public void Visit(ParameterList node)
+        {
+            int children = 0;
+            int parameters = 0;
+            foreach (var n in node)
+            {
+                children += 1;
+            }
+            if (children > 0)
+            {
+                if (visitingMain)
+                {
+                    throw new SemanticError(
+                    "Main function must not accept any variable.");
+                }
+                foreach (var n in node[0])
+                {
+                    parameters += 1;
+                }
+                if (parameters > 0)
+                {
+                    tempContainer.CustomArray.Add(parameters.ToString());
+                }
+                else
+                {
+                    tempContainer.CustomArray.Add("1");
+                }
+            }
+            else
+            {
+                tempContainer.CustomArray.Add("0");
+            }
+            tempContainer.CustomArray.Add(new LocalTable());
+            GlobalFunctionsTable.Add(tempContainer);
+            flagIsMain = false;
+        }
+
+
+
+        public void VisitChildren(Node node){
+            foreach(var n in node){
+                Visit((dynamic)n);
+            }
         }
     }
 }
